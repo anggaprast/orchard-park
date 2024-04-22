1d002f83588ca362c3f37e01b71cae42d6315d56
anggaprast <anggaprastyo010@gmail.com>
2024-04-22 16:12:58 +0700

    hamburger menu optimization

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ menu.addEventListener('click', function () {
     menu.classList.toggle('menu-active');
     navMenu.classList.toggle('hidden');
 });
+
+// Click anywhere outside sidebar to close the nav
+document.addEventListener('click', function (e) {
+    if (!menu.contains(e.target) && !navMenu.contains(e.target)) {
+        menu.classList.remove('menu-active');
+        navMenu.classList.add('hidden');
+    }
+})
